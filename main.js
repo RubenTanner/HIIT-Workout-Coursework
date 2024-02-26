89aa23f967a1bd4b4d1ff55f5d12e48cda7857f8
RubenTanner <up2109969@myport.ac.uk>
2024-02-26 13:17:05 +0000

    just a new direction

--- main.js
+++ main.js
@@ -1,199 +1,76 @@
-"use strict";
+const timerEl = document.getElementById("time");
+const statusTextEl = document.querySelector(".status-text");
+const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
 
-/// DOM ELEMENTS
-const startBtn = document.querySelector("#submit");
-const resetBtn = document.querySelector("#reset");
-const timerEl = document.querySelector("#timer");
-const cmdEl = document.querySelector("#command");
-const roundEl = document.querySelector("#round");
-const timerCircle = document.querySelector(".timer-container");
-const btnControls = document.querySelectorAll(".btn-control");
-const setAlwaysOn = document.querySelector("#screen-on");
-
-const inputsSection = document.querySelector("#inputs");
-const setTime = document.querySelector("#setTime");
-const setRes = document.querySelector("#setRest");
-const setRounds = document.querySelector("#setRounds");
-
-const messenger = function (msg, parentElement, time = 3) {
-  let msgElement = document.createElement("p");
-  msgElement.innerHTML = msg;
-  msgElement.classList.add("msgElement");
-  msgElement.classList.add("active");
-  parentElement.appendChild(msgElement);
-  parentElement.style.position = "relative";
-  setTimeout(() => {
-    msgElement.classList.remove("active");
-    msgElement.remove();
-    parentElement.style.position = "";
-  }, time * 1000);
-};
-
-// // reset settings
-// const resetValues = function () {
-//   setTime.value = 30;
-//   setRes.value = 15;
-//   setRounds.value = 5;
-// };
-// resetValues();
-
-let interval;
-let pauseInterval;
-// let time = setTime.value;
-// let rounds = setRounds.value;
-// let pauseTime = setRes.value;
 let isRunning = false;
-let isRes = false;
-let screenAlwaysOn = false;
+let intervalId;
+let timeLeft = 0;
+let activityTime = 30;
+let restTime = 10;
+let totalTime = 7 * 60;
 
-// format time
-function sec2human(seconds) {
-  sec = seconds % 60;
-  min = parseInt(seconds / 60);
-  if (sec.toString().length == 1) {
-    // padding
-    sec = "0" + sec;
-  }
-  return min + ":" + sec;
-}
-
-const timersSettings = function () {
-  time = setTime.value;
-  rounds = setRounds.value;
-  pauseTime = setRes.value;
-  timerEl.innerHTML = sec2human(time);
-  roundEl.innerHTML = rounds;
-};
-
-btnControls.forEach((btn) => {
-  if (btn.classList.contains("plus")) {
-    const plused = document.querySelector(`#${btn.previousElementSibling.id}`);
-    btn.addEventListener("click", () => {
-      plused.value = Number(plused.value) + 5;
-      plused.id === "setRounds"
-        ? (plused.value = Number(plused.value) - 4)
-        : "";
-      timersSettings();
-    });
-  }
-  if (btn.classList.contains("minus")) {
-    const plused = document.querySelector(`#${btn.nextElementSibling.id}`);
-    btn.addEventListener("click", () => {
-      if (plused.id !== "setRounds" && plused.value <= 5) {
-        plused.value = 5;
-        messenger(`Please enter higher value`, plused.parentElement, 3);
-      } else if (plused.id === "setRounds" && plused.value <= 1) {
-        plused.value = 1;
-        messenger(`Please enter higher value`, plused.parentElement, 3);
-      } else {
-        plused.value = Number(plused.value) - 5;
-        plused.id === "setRounds"
-          ? (plused.value = Number(plused.value) + 4)
-          : "";
-        timersSettings();
-      }
-    });
-  }
-});
-
-// set timers
-inputsSection.addEventListener("keyup", () => {
-  timersSettings();
-});
-inputsSection.addEventListener("change", () => {
-  timersSettings();
-});
-
-const stopTraining = function () {
-  clearInterval(pauseInterval);
-  clearInterval(interval);
-  isRunning = false;
-  time = setTime.value;
-  rounds = setRounds.value;
-  pauseTime = setRes.value;
-  startBtn.innerHTML = "start";
-  //   reset view
-  timerEl.innerHTML = sec2human(time);
-  roundEl.innerHTML = rounds;
-  timerCircle.style.background = "white";
-  timerCircle.style.color = "black";
-  cmdEl.innerHTML = "Get Ready!";
-
-  // cmdEl.innerHTML = 'Get Ready!'
-};
-
-function timer(stopFunction) {
-  // bell.play()
-  audioPlayer();
-  // start training
-  isRunning = true;
-  timerCircle.style.background = green;
-  timerCircle.style.color = "white";
-  roundEl.innerHTML = rounds;
-  cmdEl.innerHTML = "WORK!";
-  interval = setInterval(() => {
-    timerEl.innerHTML = sec2human(time);
-    time--;
-    time < 0 ? audioPlayer() : "";
-
-    // round times up
-    if (time < 0) {
-      clearInterval(interval);
-      rounds--;
-
-      // round ends and work time is not up
-      if (rounds > 0) {
-        timerCircle.style.background = "orange";
-        timerCircle.style.color = "white";
-        cmdEl.innerHTML = "REST!";
-        roundEl.innerHTML = rounds;
-        pauseInterval = setInterval(() => {
-          timerEl.innerHTML = sec2human(pauseTime);
-          pauseTime--;
-          // pause times up
-          if (pauseTime < 0) {
-            clearInterval(pauseInterval);
-            pauseTime = setRes.value;
-            time = setTime.value;
-            return timer();
-          }
-        }, 1000);
-      }
-      // training ends
-      if (rounds === 0) {
-        stopTraining();
-        cmdEl.innerHTML = "GOOD JOB!";
-      }
-    }
-  }, 1000);
-}
-
-// buttons
-startBtn.addEventListener("click", () => {
-  !isRunning ? timer(stopTraining) : stopTraining();
-  !isRunning ? (startBtn.innerHTML = "Start") : (startBtn.innerHTML = "Finish");
-});
-
-resetBtn.addEventListener("click", () => {
+function startTimer() {
   if (!isRunning) {
-    resetValues();
-    timersSettings();
-  } else {
-    //    msgeer function
-    messenger(
-      `Can't reset while running`,
-      document.querySelector(".buttons"),
-      2
-    );
-  }
-});
-//  circle click
-timerCircle.addEventListener("click", () => {
-  !isRunning ? timer(stopTraining) : stopTraining();
-  !isRunning ? (startBtn.innerHTML = "Start") : (startBtn.innerHTML = "Finish");
-});
+    isRunning = true;
+    timeLeft = activityTime;
+    statusTextEl.innerText = "Activity";
+    document.body.style.backgroundColor = "#ff5722";
 
-soundCheckbox.addEventListener("change", () => {
-  console.log(soundCheckbox.checked);
-  soundCheckbox.checked ? (soundOnOff = true) : (soundOnOff = false);
+    intervalId = setInterval(() => {
+      timeLeft--;
+
+      if (timeLeft <= 0) {
+        beep.play();
+
+        if (statusTextEl.innerText === "Activity") {
+          statusTextEl.innerText = "Rest";
+          document.body.style.backgroundColor = "#007bff";
+          timeLeft = restTime;
+        } else {
+          statusTextEl.innerText = "Activity";
+          document.body.style.backgroundColor = "#ff5722";
+          timeLeft = activityTime;
+        }
+      }
+
+      updateTimer();
+    }, 1000);
+  } else {
+    stopTimer();
+  }
+}
+
+function stopTimer() {
+  clearInterval(intervalId);
+  isRunning = false;
+  timeLeft = 0;
+  statusTextEl.innerText = "Activity";
+  timerEl.innerText = "00:00";
+  document.body.style.backgroundColor = "#f5f5f5";
+}
+
+function updateTimer() {
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+  timerEl.innerText = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+function pauseTimer() {
+  if (isRunning) {
+    clearInterval(intervalId);
+    isRunning = false;
+  }
+}
+
+startBtn.addEventListener("click", () => {
+  if (!isRunning) {
+    startTimer();
+    startBtn.innerText = "Pause"; // Change the text to "Pause"
+  } else {
+    pauseTimer();
+    startBtn.innerText = "Start"; // Change the text back to "Start"
+  }
 });
